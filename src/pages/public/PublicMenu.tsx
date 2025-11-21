eeee702f0c8c9b2be54fe3e0b4c788c5f3a4d958
digitalfenixpro <admin@digitalfenixpro.com>
2025-11-20 21:01:32 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -769,7 +769,7 @@ export const PublicMenu: React.FC = () => {
               viewMode === 'list'
                 ? 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4' /*DF: agregar scale para reducir un poco el tamaño*/
                 : viewMode === 'grid'
-                ? 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'
+                ? 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4'
                 : 'space-y-2'
             }
           >
