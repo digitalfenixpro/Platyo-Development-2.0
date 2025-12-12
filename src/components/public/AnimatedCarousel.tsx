134af333ee17ffc896d01604a9f578f4397978ba
digitalfenixpro <admin@digitalfenixpro.com>
2025-12-12 10:19:33 -0500

    Updated AnimatedCarousel.tsx

--- src/components/public/AnimatedCarousel.tsx
+++ src/components/public/AnimatedCarousel.tsx
@@ -18,7 +18,7 @@ export const AnimatedCarousel: React.FC<AnimatedCarouselProps> = ({
   products,
   primaryColor,
   secundaryColor,
-  textColor,
+  textColor = {secondaryTextColor},
   cardBackgroundColor = '#ffffff',
   fontFamily = 'Poppins',
   onProductClick,
